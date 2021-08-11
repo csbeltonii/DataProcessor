a2cbad6a5f01bed77933e63034c20b07bae2229f
csbeltonii <csbeltonii@gmail.com>
2021-08-10 22:20:55 -0400

    first commot

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Concurrent;
+using static System.Console;
+using System.IO;
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        public string InputFilePath { get; }
+
+        private static readonly string BackupDirectoryName = "backup";
+        private static readonly string InProgressDirectoryName = "processing";
+        private static readonly string CompletedDirectoryName = "complete";
+
+        public FileProcessor(string filePath)
+        {
+            InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            WriteLine($"Begin process of {InputFilePath}");
+
+            // Check if file exists
+            if (!File.Exists(InputFilePath))
+            {
+                WriteLine($"ERROR: File {InputFilePath} does not exist.");
+                return;
+            }
+
+            var rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
+
+            WriteLine($"Root data path is {rootDirectoryPath}");
+
+            // check if backup dir exists
+            var inputFileDirectoryPath = Path.GetDirectoryName(InputFilePath);
+            var backupDirectoryPath = Path.Combine(rootDirectoryPath, BackupDirectoryName);
+            
+            Directory.CreateDirectory(backupDirectoryPath);
+
+            // copy file to backup
+            var inputFileName = Path.GetFileName(InputFilePath);
+
+            var backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+
+            WriteLine($"Copying {inputFileName} to {backupFilePath}");
+
+            File.Copy(InputFilePath, backupFilePath, true);
+
+            // move to progress dir
+            Directory.CreateDirectory(Path.Combine(rootDirectoryPath, InProgressDirectoryName));
+
+            var inProgressFilePath = Path.Combine(rootDirectoryPath, InProgressDirectoryName, inputFileName);
+
+            if (File.Exists(inProgressFilePath))
+            {
+                WriteLine($"ERROR: A file with name{inProgressFilePath} is already being processed.");
+                return;
+            }
+
+            WriteLine($"Moving {InputFilePath} to {inProgressFilePath}");
+            File.Move(InputFilePath, inProgressFilePath);
+
+            // Determine type of file
+            var extension = Path.GetExtension(InputFilePath);
+
+            switch (extension)
+            {
+                case ".txt":
+                    ProcessTextFile(inProgressFilePath);
+                    break;
+                default:
+                    WriteLine($"{extension} is an unsupported file type.");
+                    break;
+            }
+
+            var completedDirectoryPath = Path.Combine(rootDirectoryPath, CompletedDirectoryName);
+            Directory.CreateDirectory(completedDirectoryPath);
+
+            WriteLine($"Moving {inProgressFilePath} to {completedDirectoryPath}");
+            //File.Move(inProgressFilePath, Path.Combine(completedDirectoryPath, inputFileName));
+
+            var completedFileName = $"{Path.GetFileNameWithoutExtension(InputFilePath)}--{Guid.NewGuid()}{extension}";
+
+            var completedFilePath = Path.Combine(completedDirectoryPath, completedFileName);
+
+            File.Move(inProgressFilePath, completedFilePath);
+
+            var inProgressDirectoryPath = Path.GetDirectoryName(inProgressFilePath);
+
+            Directory.Delete(inProgressDirectoryPath, true);
+        }
+
+        private void ProcessTextFile(string inProgressFilePath)
+        {
+            WriteLine($"Processing text file {inProgressFilePath}");
+        }
+    }
+}
---
+++ DataProcessor/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+using System.IO;
+using System.Runtime.Caching;
+using System.Threading;
+
+namespace DataProcessor
+{
+    class Program
+    {
+
+        //private static ConcurrentDictionary<string, string> FilesToProcess = new ConcurrentDictionary<string, string>();
+
+        private static MemoryCache FilesToProcess = MemoryCache.Default;
+
+        static void Main(string[] args)
+        {
+            WriteLine("Parsing command line options.");
+
+            //var command = args[0];
+
+            //if (command == "--file")
+            //{
+            //    var filePath = args[1];
+            //    WriteLine($"Single file {filePath} selected");
+            //    ProcessSingleFile(filePath);
+            //}
+            //else if (command == "--dir")
+            //{
+            //    var directoryPath = args[1];
+            //    var fileType = args[2];
+            //    WriteLine($"Directory {directoryPath} selected for {fileType} files.");
+            //    ProcessDirectory(directoryPath, fileType);
+            //}
+            //else
+            //{
+            //    WriteLine("Invalid command line option.");
+            //}
+
+            //WriteLine("Please enter to quit.");
+            //ReadLine();
+
+            var directoryToWatch = args[0];
+
+            if (!Directory.Exists(directoryToWatch))
+            {
+                WriteLine($"ERROR: {directoryToWatch} does not exist.");
+            }
+            else
+            {
+                WriteLine($"Watching directory {directoryToWatch} for changes");
+
+                using var inputFileWatch = new FileSystemWatcher(directoryToWatch);
+
+                inputFileWatch.IncludeSubdirectories = false;
+                inputFileWatch.InternalBufferSize = 32768; // 32 KB
+                inputFileWatch.Filter = "*.*"; // default filter
+                inputFileWatch.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+
+                inputFileWatch.Created += FileCreated;
+                inputFileWatch.Changed += FileChanged;
+                inputFileWatch.Deleted += FileDeleted;
+                inputFileWatch.Renamed += FileRenamed;
+                inputFileWatch.Error += WatcherError;
+
+                inputFileWatch.EnableRaisingEvents = true;
+
+                WriteLine("Press enter to quit.");
+                ReadLine();
+            }
+        }
+
+        private static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            WriteLine($"* File created: {e.Name} - type: {e.ChangeType}");
+
+            //var fileProcessor = new FileProcessor(e.FullPath);
+            //fileProcessor.Process();
+
+            AddToCache(e.FullPath);
+        }
+
+        private static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            WriteLine($"* File changed: {e.Name} - type: {e.ChangeType}");
+
+            //var fileProcessor = new FileProcessor(e.FullPath);
+            //fileProcessor.Process();
+
+            AddToCache(e.FullPath);
+        }
+
+        private static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            WriteLine($"* File delete: {e.Name} - type: {e.ChangeType}");
+        }
+
+        private static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            WriteLine($"* File renamed: {e.OldName} to {e.Name} - type: {e.ChangeType}");
+        }
+
+        private static void WatcherError(object sender, ErrorEventArgs e)
+        {
+            WriteLine($"ERROR: file system watching may no longer be action: {e.GetException()}");
+        }
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            //var allFiles = Directory.GetFiles(directoryPath); // files
+
+            switch (fileType)
+            {
+                case "TEXT":
+                    var textFile = Directory.GetFiles(directoryPath, "*.txt");
+
+                    foreach (var textFilePath in textFile)
+                    {
+                        var fileProcessor = new FileProcessor(textFilePath);
+                        fileProcessor.Process();
+                    }
+                    break;
+                default:
+                    WriteLine($"ERROR: {fileType} is not supported.");
+                    return;
+            }
+        }
+
+        //private static void ProcessFiles(Object stateInfo)
+        //{
+        //    foreach (var fileName in FilesToProcess.Keys)
+        //    {
+        //        if (FilesToProcess.TryRemove(fileName, out _))
+        //        {
+        //            var fileProcessor = new FileProcessor(fileName);
+        //            fileProcessor.Process();
+        //        }
+        //    }
+        //}
+
+        private static void AddToCache(string fullPath)
+        {
+            var item = new CacheItem(fullPath, fullPath);
+
+            var policy = new CacheItemPolicy
+            {
+                RemovedCallback = ProcessFile,
+                SlidingExpiration = TimeSpan.FromSeconds(2)
+            };
+
+            FilesToProcess.Add(item, policy);
+        }
+
+        private static void ProcessFile(CacheEntryRemovedArguments args)
+        {
+            WriteLine($"* Cache item removed: {args.CacheItem.Key} because {args.RemovedReason}.");
+
+            if (args.RemovedReason == CacheEntryRemovedReason.Expired)
+            {
+                var fileProcessor = new FileProcessor(args.CacheItem.Key);
+                fileProcessor.Process();
+            }
+            else
+            {
+                WriteLine($"WARNING: {args.CacheItem.Key} was removed unexpectedly and may not klhjlkj.");
+            }
+        }
+    }
+}
